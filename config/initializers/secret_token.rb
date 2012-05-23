4069fc210fc796006dc04cf0c5c63779e07b0c54
stillunnamed <stillunnamed.42@gmail.com>
2012-05-23 17:38:02 -0400

    Inital Commit.
    Following: http://ruby.railstutorial.org/chapters/static-pages#top
    
    This is the first page for the big 'sample app', and it will start
    with adding the static pages to the site.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0e6fe393bc7239a4c4bcdca0c4f44782dd1ec1163a067ebc595c38398de6d6f699f0b732669feabf9dbf394e2cdc66b83ee09d47a3251a2c192b13e7bd0d79ac'
